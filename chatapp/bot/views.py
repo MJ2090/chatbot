453c3f541bb2d16f600f48ae8b0d8499e59147f3
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-21 10:48:22 -0700

    s

--- chatapp/bot/views.py
+++ chatapp/bot/views.py
@@ -8,5 +8,13 @@ def home(request):
 
 
 def test(request):
-    ret = {'msg': 'Hi Tester.'}
+    new_msg = """
+    what is Done??
+    """
+    history_msg = """
+    [{"role":"user","content":"Hello! How can I assist you today?"},{"role":"assistant","content":"i feel happy today"},{"role":"user","content":"That's great to hear! Is there anything specific that made you feel happy today?"}]
+    """
+    request_post_data = {'message': new_msg, 'history': history_msg}
+    output = handle_chat(request_post_data)
+    ret = {'msg': output}
     return render(request, 'bot/home.html', ret)
