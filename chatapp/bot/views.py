8d063baecc857617e7aad463d2e0cf53b24ff50f
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-21 10:27:32 -0700

    s

--- chatapp/bot/views.py
+++ chatapp/bot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    ret = {'msg': 'Hi Done.'}
+    return render(request, 'embedding/home.html', ret)
+
+
+def test(request):
+    ret = {'msg': 'Hi Tester.'}
+    return render(request, 'embedding/home.html', ret)
