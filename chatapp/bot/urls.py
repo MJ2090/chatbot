3304563ac68f9c445d8228683088f0e99a39327b
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-04-22 14:22:54 -0700

    s

--- chatapp/bot/urls.py
+++ chatapp/bot/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('test/', views.test, name='test'),
+    path('test_json/', views.test_json, name='test_json'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- chatapp/bot/views.py
+++ chatapp/bot/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 from main import handle_chat
 
 
@@ -18,3 +20,15 @@ def test(request):
     output = handle_chat(request_post_data)
     ret = {'msg': output}
     return render(request, 'bot/home.html', ret)
+
+def test_json(request):
+    new_msg = """
+    what is Done??
+    """
+    history_msg = """
+    [{"role":"user","content":"Hello! How can I assist you today?"},{"role":"assistant","content":"i feel happy today"},{"role":"user","content":"That's great to hear! Is there anything specific that made you feel happy today?"}]
+    """
+    request_post_data = {'message': new_msg, 'history': history_msg}
+    output = handle_chat(request_post_data)
+    ret = {'msg': output}
+    return HttpResponse(json.dumps({'ai_message': output}))
